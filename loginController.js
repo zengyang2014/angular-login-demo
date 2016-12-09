ae5c122ede4f77e72894768fc103f2b83fa31dd0
xinzhang <xinzhang@thoughtworks.com>
2016-12-09 15:22:17 +0800

    Use es6

--- loginController.js
+++ loginController.js
@@ -1,15 +1,14 @@
 var app = angular.module('loginModule', []);
 
-
 var initUsername = 'admin';
 var initPassword = '123456';
 
-app.controller('loginCtrl', function($scope){
-	$scope.loginHandler = function() {
+app.controller('loginCtrl', ($scope) => {
+	$scope.loginHandler = () => {
 		if($scope.username == initUsername && $scope.password == initPassword) {
 			$scope.result = 'Login Success';
 		} else {
 			$scope.result = 'Login Fail';
 		}
 	}
-})
+});
