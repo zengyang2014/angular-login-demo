6b24be89f28ae6c049418cdd63db24687588fbb8
xinzhang <xinzhang@thoughtworks.com>
2016-12-14 22:54:53 +0800

    update location select

--- loginController.js
+++ loginController.js
@@ -3,6 +3,14 @@ var app = angular.module('loginModule', []);
 var initUsername = 'admin';
 var initPassword = '123456';
 
+var locationSel = [
+	'Select Location',
+	'Registraction Desk',
+	'OPD-1',
+	'General Ward',
+	'Labour Ward'
+];
+
 app.controller('loginCtrl', ($scope) => {
 	$scope.loginHandler = () => {
 		if ($scope.username == initUsername && $scope.password == initPassword) {
@@ -11,4 +19,5 @@ app.controller('loginCtrl', ($scope) => {
 			$scope.result = 'You are not authenticated or your session expired. Please login.';
 		}
 	}
+	$scope.locations = locationSel;
 });
